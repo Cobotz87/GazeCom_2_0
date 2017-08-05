5af80c9a19b702496874859289702364904f1e7f
cobotz <cobotz87@gmail.com>
2017-08-05 16:25:50 +0800

    Add Fixation Activator class

--- GazeCom_2_0/MainWindow.xaml.cs
+++ GazeCom_2_0/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GazeCom_2_0.TobiiActivator;
 using Tobii.Interaction;
 using Tobii.Interaction.Wpf;
 
@@ -22,34 +23,19 @@ namespace GazeCom_2_0
     /// </summary>
     public partial class MainWindow : Window
     {
-        private App _appInstance;
-
-        //Tobii
-        private static Host _tobiiHost;
-
-        //Tobii, fixation
-        private static bool _fixationStart;
-        private static double _fixationBeginTimeStamp;
-        private static double _fixationActivationDuration;
-
         public MainWindow()
         {
-            //This
-            _fixationStart = false;
-            _fixationBeginTimeStamp = 0;
-            _fixationActivationDuration = 500;
-
             //Tobii
-            _appInstance = Application.Current as App;
-            _tobiiHost = _appInstance?.TobiiHost;
+            var appInstance = Application.Current as App;
+            var tobiiHost = appInstance?.TobiiHost;
 
-            var gazePointDataStream = _tobiiHost.Streams.CreateGazePointDataStream();
+            //FixationHelper
+            var fixationActivator = new FixationActivator(ref tobiiHost);
+            fixationActivator.OnActivate += OnFixationActivation;
+
+            //Tobii, Gaze Data Stream
+            var gazePointDataStream = tobiiHost.Streams.CreateGazePointDataStream();
             gazePointDataStream.GazePoint(OnGazePointDataStream);
-
-            var fixationDataStream = _tobiiHost.Streams.CreateFixationDataStream();
-            fixationDataStream.Begin(OnFixationDataStreamBegin);
-            fixationDataStream.Data(OnFixationDataStreamData);
-            fixationDataStream.End(OnFixationDataStreamEnd);
 
             InitializeComponent();
         }
@@ -60,55 +46,26 @@ namespace GazeCom_2_0
             
         }
 
-        //Tobii Fixation, Begin
-        private static void OnFixationDataStreamBegin(double x, double y, double timeStamp)
+        //Tobii, Fixation
+        private static void OnFixationActivation(ref Host tobiiHost)
         {
-            _fixationStart = true;
-            _fixationBeginTimeStamp = timeStamp;
+            tobiiHost.Commands.Input.SendActivation();
         }
-
-        //Tobii Fixation, Data
-        private static bool IsFixationActivated(double timeStamp)
-        {
-            return timeStamp - _fixationBeginTimeStamp > _fixationActivationDuration;
-        }
-
-        private static void OnFixationDataStreamData(double x, double y, double timeStamp)
-        {
-            if (!IsFixationActivated(timeStamp))
-                return;
-
-            _fixationBeginTimeStamp = 0;
-
-            if (!_fixationStart)
-                return;
-
-            _tobiiHost.Commands.Input.SendActivation();
-            _fixationStart = false;
-        }
-
-        //Tobii Fixation, End
-        private static void OnFixationDataStreamEnd(double x, double y, double timeStamp)
-        {
-            _fixationStart = false;
-            _fixationBeginTimeStamp = 0;
-        }
-
         
         //Activation Events
         private void BtnYes_OnActivated(object sender, ActivationRoutedEventArgs e)
         {
-            MessageBox.Show("BtnYes_OnActivated is indeed activated!");
+            MessageBox.Show("BtnYes_OnActivated is indeed activated!!");
         }
 
         private void BtnNo_OnActivated(object sender, ActivationRoutedEventArgs e)
         {
-            MessageBox.Show("BtnNo_OnActivated is indeed activated!");
+            MessageBox.Show("BtnNo_OnActivated is indeed activated!!");
         }
 
         private void BtnMaybe_OnActivated(object sender, ActivationRoutedEventArgs e)
         {
-            MessageBox.Show("BtnMaybe_OnActivated is indeed activated!");
+            MessageBox.Show("BtnMaybe_OnActivated is indeed activated!!");
         }
     }
 }
---
+++ GazeCom_2_0/TobiiActivator/FixationActivator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tobii.Interaction;
+
+namespace GazeCom_2_0.TobiiActivator
+{
+    public class FixationActivator
+    {
+        //Delegates
+        public delegate void OnActivateDelegate(ref Host tobiiHost);
+        public OnActivateDelegate OnActivate;
+
+        //Tobii
+        private static Host _tobiiHost;
+
+        //Tobii, fixation
+        private bool _fixationStart;
+        private double _fixationBeginTimeStamp;
+        private readonly double _fixationActivationDuration;
+
+        public FixationActivator(ref Host tobiiHost)
+        {
+            _tobiiHost = tobiiHost;
+
+            _fixationStart = false;
+            _fixationBeginTimeStamp = 0;
+            _fixationActivationDuration = 500; //miliseconds
+
+            var fixationDataStream = tobiiHost.Streams.CreateFixationDataStream();
+            fixationDataStream.Begin(OnFixationDataStreamBegin);
+            fixationDataStream.Data(OnFixationDataStreamData);
+            fixationDataStream.End(OnFixationDataStreamEnd);
+        }
+
+        //Tobii Fixation, Begin
+        private void OnFixationDataStreamBegin(double x, double y, double timeStamp)
+        {
+            _fixationStart = true;
+            _fixationBeginTimeStamp = timeStamp;
+        }
+
+        //Tobii Fixation, Data
+        private bool IsFixationActivated(double timeStamp)
+        {
+            return timeStamp - _fixationBeginTimeStamp > _fixationActivationDuration;
+        }
+
+        private void OnFixationDataStreamData(double x, double y, double timeStamp)
+        {
+            if (!IsFixationActivated(timeStamp))
+                return;
+
+            _fixationBeginTimeStamp = 0;
+
+            if (!_fixationStart)
+                return;
+
+            OnActivate?.Invoke(ref _tobiiHost);
+            _fixationStart = false;
+        }
+
+        //Tobii Fixation, End
+        private void OnFixationDataStreamEnd(double x, double y, double timeStamp)
+        {
+            _fixationStart = false;
+            _fixationBeginTimeStamp = 0;
+        }
+
+
+    }
+}
