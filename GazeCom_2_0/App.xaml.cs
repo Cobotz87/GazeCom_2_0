fde76db7004b795026564371f036159a16505ef8
cobotz <cobotz87@gmail.com>
2017-08-05 16:11:49 +0800

    Initial Commit

---
+++ GazeCom_2_0/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Tobii.Interaction;
+using Tobii.Interaction.Wpf;
+
+namespace GazeCom_2_0
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public Host TobiiHost { get; private set; }
+        private WpfInteractorAgent _wpfInteractorAgent;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            TobiiHost = new Host();
+            _wpfInteractorAgent = TobiiHost.InitializeWpfAgent();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            TobiiHost.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ GazeCom_2_0/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tobii.Interaction;
+
+namespace GazeCom_2_0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private App _appInstance;
+        private Host _tobiiHost;
+
+        public MainWindow()
+        {
+            _appInstance = Application.Current as App;
+            _tobiiHost = _appInstance?.TobiiHost;
+
+            var gazePointDataStream = _tobiiHost.Streams.CreateGazePointDataStream();
+            gazePointDataStream.GazePoint(OnGazePointDataStream);
+
+            InitializeComponent();
+        }
+
+        private static void OnGazePointDataStream(double x, double y, double timeStamp)
+        {
+            
+        }
+    }
+}
